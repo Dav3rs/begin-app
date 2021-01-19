27f4f1dc143d2490eb05af370d8c03f1339a5fbf
Dav3rs <davidiazfz@gmail.com>
2021-01-18 22:49:39 -0500

    index changes

--- src/http/get-index/index.ts
+++ src/http/get-index/index.ts
@@ -1,37 +1,41 @@
-// TODO: modify the body object!
-let body = `
+function quote() {
+    let min = 0
+    let max = quotes.length - 1
+    let random = ~~(Math.random() * (max - min) + min)
+    return quotes[random]
+}
+
+let quotes = [
+    "It's a family that's loaded with grudges and passion.",
+    "To be a good actor you have to be something like a criminal",
+    "I think I jump around more when I'm alone.",
+    "I bought a Yamaha-1 and I was doing 180 miles per hour home",
+]
+
+export async function handler(req: object) {
+    return {
+        statusCode: 200,
+        headers: {
+            'content-type': 'text/html; charset=utf8',
+            'cache-control': 'no-cache, no-store, must-revalidate, max-age=0, s-maxage=0'
+        },
+        body: `
 <!doctype html>
 <html lang=en>
   <head>
     <meta charset=utf-8>
     <title>Hi!</title>
-    <link rel="stylesheet" href="https://static.begin.app/starter/default.css">
-    <link href="data:image/x-icon;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkYAAAAAYAAjCB0C8AAAAASUVORK5CYII=" rel="icon" type="image/x-icon">
   </head>
   <body>
 
-    <h1 class="center-text">
-      <!-- ↓ Change "Hello world!" to something else and head on back to Begin! -->
-      Hello world!
+    <h1 style="text-align: center;">
+      ${quote()}
     </h1>
-
-    <p class="center-text">
-      Your <a href="https://begin.com" class="link" target="_blank">Begin</a> app is ready to go!
-    </p>
 
   </body>
 </html>
 `
-
-export async function handler (req: object) {
-  return {
-    statusCode: 200,
-    headers: {
-      'content-type': 'text/html; charset=utf8',
-      'cache-control': 'no-cache, no-store, must-revalidate, max-age=0, s-maxage=0'
-    },
-    body
-  }
+    }
 }
 
 // Example responses
